75d31f564f7f79c39a4155f1339d5ba7c90ed486
Willian Pitter Cardoso Lima <45232359+Willianpitter@users.noreply.github.com>
2024-09-10 23:59:40 +0000

    simple deploy

---
+++ 04- deployment/web-service/README.md
@@ -0,0 +1,18 @@
+## Deploying a model as a web-service
+
+* Creating a virtual environment with Pipenv
+* Creating a script for predictiong 
+* Putting the script into a Flask app
+* Packaging the app to Docker
+
+```bash
+gunicorn --bind=0.0.0.0:9696 predict:app
+```
+
+```bash
+docker build -t ride-duration-prediction-service:v1 .
+```
+
+```bash
+docker run -it --rm -p 9696:9696  ride-duration-prediction-service:v1
+```
---
+++ 04- deployment/web-service/predict.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask, request, jsonify
+with open('lin_reg.bin', 'rb') as f_in:
+    (dv, model) = pickle.load(f_in)
+
+# To run on gunicorn: gunicorn --bind=0.0.0.0:9696 predict:app
+app = Flask('duration-prediction')
+def prepare_features(ride):
+    features = {}
+    features['PU_DO'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+def predict(features):
+    X = dv.transform(features)
+    preds = model.predict(X)
+    return preds[0]
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+    features = prepare_features(ride)
+    pred = predict(features)
+    result = {
+        'duration': pred
+    }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696) 
